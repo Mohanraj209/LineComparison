dde949e30e8a5619776c6a06adca6118bdf05fde
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-16 00:06:55 +0530

    Displaying Message

---
+++ LineComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("----------Welcome to Line Comparison Computation Program---------");
+            LineComp.Cal_Length();
+        }
+    }
+}
